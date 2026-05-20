fd98b3eec6c074c5c702e763e25b4546b21fbac9
Jim Ramsay <jramsay@redhat.com>
2026-05-20 10:54:18 -0400

    Fixup formatting in daemon.go
    Signed-off-by: Jim Ramsay <jramsay@redhat.com>

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -167,7 +167,8 @@ func NewProcessManager() *ProcessManager {
 
 // SetTestProfileProcess ...
 func (p *ProcessManager) SetTestProfileProcess(name string, ifaces config.IFaces, socketPath,
-	processConfigPath string, nodeProfile ptpv1.PtpProfile) {
+	processConfigPath string, nodeProfile ptpv1.PtpProfile,
+) {
 	p.process = append(p.process, &ptpProcess{
 		name:              name,
 		ifaces:            ifaces,
@@ -228,7 +229,6 @@ func (p *ProcessManager) UpdateSynceConfig(config *synce.Relations) {
 		return
 	}
 	p.process[0].syncERelations = config
-
 }
 
 // EmitProcessStatusLogs emits process status logs using the EventHandler's
@@ -493,7 +493,7 @@ func New(
 		// Watch the known security mount folder from startup
 		if watchErr := saFileWatch.Add(PtpSecretMountDir); watchErr != nil {
 			glog.Warningf("Failed to watch %s (may not exist yet): %v", PtpSecretMountDir, watchErr)
-			//destroy and release the watcher
+			// destroy and release the watcher
 			saFileWatch.Close()
 			saFileWatch = nil
 		} else {
@@ -666,7 +666,7 @@ func (dn *Daemon) applyNodePTPProfiles() error {
 	// compare nodeProfile with previous config,
 	// only apply when nodeProfile changes
 
-	//clear hwconfig before updating
+	// clear hwconfig before updating
 	*dn.hwconfigs = []ptpv1.HwConfig{}
 
 	glog.Infof("updating NodePTPProfiles to:")
@@ -674,7 +674,7 @@ func (dn *Daemon) applyNodePTPProfiles() error {
 	slices.SortFunc(dn.ptpUpdate.NodeProfiles, func(a, b ptpv1.PtpProfile) int {
 		aHasPhc2sysOpts := a.Phc2sysOpts != nil && *a.Phc2sysOpts != ""
 		bHasPhc2sysOpts := b.Phc2sysOpts != nil && *b.Phc2sysOpts != ""
-		//sorted in ascending order
+		// sorted in ascending order
 		// here having phc2sysOptions is considered a high number
 		if !aHasPhc2sysOpts && bHasPhc2sysOpts {
 			return -1 //  a<b return -1
@@ -741,33 +741,28 @@ func (dn *Daemon) applyNodePTPProfiles() error {
 	for _, p := range dn.processManager.process {
 		if p != nil {
 			p.eventCh = dn.processManager.eventChannel
-			// start ptp4l process early , it doesn't have
-			if p.depProcess == nil {
-				go p.cmdRun(dn.stdoutToSocket, &dn.pluginManager)
-			} else {
-				for _, d := range p.depProcess {
-					if d != nil {
-						time.Sleep(3 * time.Second)
-						glog.Infof("Starting %s", d.Name())
-						go d.CmdRun(false)
-						time.Sleep(3 * time.Second)
-						dn.pluginManager.AfterRunPTPCommand(&p.nodeProfile, d.Name())
-						d.MonitorProcess(config.ProcessConfig{
-							ClockType:    p.clockType,
-							ConfigName:   p.configName,
-							EventChannel: dn.processManager.eventChannel,
-							GMThreshold: config.Threshold{
-								Max:             p.ptpClockThreshold.MaxOffsetThreshold,
-								Min:             p.ptpClockThreshold.MinOffsetThreshold,
-								HoldOverTimeout: p.ptpClockThreshold.HoldOverTimeout,
-							},
-							InitialPTPState: event.PTP_FREERUN,
-						})
-						glog.Infof("enabling dep process %s with Max %d Min %d Holdover %d", d.Name(), p.ptpClockThreshold.MaxOffsetThreshold, p.ptpClockThreshold.MinOffsetThreshold, p.ptpClockThreshold.HoldOverTimeout)
-					}
+			for _, d := range p.depProcess {
+				if d != nil {
+					time.Sleep(3 * time.Second)
+					glog.Infof("Starting %s", d.Name())
+					go d.CmdRun(false)
+					time.Sleep(3 * time.Second)
+					dn.pluginManager.AfterRunPTPCommand(&p.nodeProfile, d.Name())
+					d.MonitorProcess(config.ProcessConfig{
+						ClockType:    p.clockType,
+						ConfigName:   p.configName,
+						EventChannel: dn.processManager.eventChannel,
+						GMThreshold: config.Threshold{
+							Max:             p.ptpClockThreshold.MaxOffsetThreshold,
+							Min:             p.ptpClockThreshold.MinOffsetThreshold,
+							HoldOverTimeout: p.ptpClockThreshold.HoldOverTimeout,
+						},
+						InitialPTPState: event.PTP_FREERUN,
+					})
+					glog.Infof("enabling dep process %s with Max %d Min %d Holdover %d", d.Name(), p.ptpClockThreshold.MaxOffsetThreshold, p.ptpClockThreshold.MinOffsetThreshold, p.ptpClockThreshold.HoldOverTimeout)
 				}
-				go p.cmdRun(dn.stdoutToSocket, &dn.pluginManager)
 			}
+			go p.cmdRun(dn.stdoutToSocket, &dn.pluginManager)
 			dn.pluginManager.AfterRunPTPCommand(&p.nodeProfile, p.name)
 		}
 	}
@@ -837,7 +832,8 @@ func (dn *Daemon) applyNodePtpProfile(runID int, nodeProfile *ptpv1.PtpProfile) 
 			if _, registered := dn.pluginManager.Plugins[pluginName]; !registered {
 				pluginErrors = append(pluginErrors, fmt.Errorf(
 					"unknown plugin '%s' in profile '%s' (possible typo in hardware plugin configuration)",
-					pluginName, *nodeProfile.Name))
+					pluginName, *nodeProfile.Name,
+				))
 			}
 		}
 	}
@@ -964,7 +960,7 @@ func (dn *Daemon) applyNodePtpProfile(runID int, nodeProfile *ptpv1.PtpProfile) 
 							*configOpts += " --servo_offset_threshold " + strconv.FormatInt(min(maxInSpecOffset, maxHoldoverOffSet), 10)
 						}
 					}
-					if !strings.Contains(*configOpts, "--servo_num_offset_values") { //if consecutive smaller offsets (less than the threshold) are not observed, the system stays in S2
+					if !strings.Contains(*configOpts, "--servo_num_offset_values") { // if consecutive smaller offsets (less than the threshold) are not observed, the system stays in S2
 						*configOpts += " --servo_num_offset_values 10"
 					}
 				}
@@ -1012,7 +1008,7 @@ func (dn *Daemon) applyNodePtpProfile(runID int, nodeProfile *ptpv1.PtpProfile) 
 			output.profile_name = *nodeProfile.Name
 		}
 
-		//output, messageTag, socketPath, GPSPIPE_SERIALPORT, update_leapfile, os.Getenv("NODE_NAME")
+		// output, messageTag, socketPath, GPSPIPE_SERIALPORT, update_leapfile, os.Getenv("NODE_NAME")
 
 		// This adds the flags needed for monitor
 		addFlagsForMonitor(pProcess, configOpts, output, dn.stdoutToSocket)
@@ -1060,8 +1056,10 @@ func (dn *Daemon) applyNodePtpProfile(runID int, nodeProfile *ptpv1.PtpProfile) 
 			haProfile:         haProfile,
 			syncERelations:    relations,
 			logParser:         getParser(pProcess),
-			tBCAttributes: tBCProcessAttributes{ttPortsConfigFile: controlledConfigFile, trPortsConfigFile: configFile,
-				lastReportedState: event.PTP_NOTSET, lastAppliedState: event.PTP_NOTSET, offsetFilter: nil},
+			tBCAttributes: tBCProcessAttributes{
+				ttPortsConfigFile: controlledConfigFile, trPortsConfigFile: configFile,
+				lastReportedState: event.PTP_NOTSET, lastAppliedState: event.PTP_NOTSET, offsetFilter: nil,
+			},
 			handler: dn.processManager.ptpEventHandler,
 			dn:      dn,
 		}
@@ -1275,7 +1273,7 @@ func (dn *Daemon) applyNodePtpProfile(runID int, nodeProfile *ptpv1.PtpProfile) 
 				}
 			}
 		}
-		err = os.WriteFile(configPath, []byte(configOutput), 0644)
+		err = os.WriteFile(configPath, []byte(configOutput), 0o644)
 		if err != nil {
 			printNodeProfile(nodeProfile)
 			return fmt.Errorf("failed to write the configuration file named %s: %v", configPath, err)
@@ -1700,7 +1698,7 @@ func (p *ptpProcess) processPTPMetrics(output string) {
 				}
 			}
 			// ts2phc has to be handled differently since it announce holdover state when gnss is lost
-			//TODO: verify how 1pps is handled when lost
+			// TODO: verify how 1pps is handled when lost
 			switch clockState {
 			case FREERUN:
 				state = event.PTP_FREERUN
@@ -1756,13 +1754,13 @@ func (p *ptpProcess) cmdSetEnabled(enabled bool) {
 			_, _ = exec.Command("chronyc", "-h", ChronydSocketPath, "offline").Output()
 			processStatus(p.c, p.name, p.messageTag, PtpProcessDown)
 		}
-	case "phc2sys":
+	default:
 		if enabled {
 			if p.Stopped() && p.cmd != nil {
 				cmd := p.cmd
 				newCmd := exec.Command(cmd.Args[0], cmd.Args[1:]...)
 				p.cmd = newCmd
-				go p.cmdRun(p.dn.stdoutToSocket, &(p.dn.pluginManager))
+				go p.cmdRun(p.dn.stdoutToSocket, &p.dn.pluginManager)
 			}
 		} else {
 			p.cmdStop()
@@ -1801,7 +1799,7 @@ func (p *ptpProcess) ProcessTs2PhcEvents(ptpOffset float64, source string, iface
 	}
 
 	if source == ts2phcProcessName { // for ts2phc send it to event to create metrics and events
-		var values = make(map[event.ValueType]interface{})
+		values := make(map[event.ValueType]interface{})
 
 		values[event.OFFSET] = ptpOffsetInt64
 		for k, v := range extraValue {
@@ -2125,7 +2123,7 @@ func (p *ptpProcess) ProcessSynceEvents(logEntry synce.LogEntry) {
 					}
 
 					state = sDeviceConfig.LastClockState
-				} else if logEntry.QL != synce.QL_DEFAULT_SSM { //else we have only QL
+				} else if logEntry.QL != synce.QL_DEFAULT_SSM { // else we have only QL
 					lastQLState.SSM = logEntry.QL // wait for extTlv
 				}
 			}
@@ -2155,8 +2153,8 @@ func (p *ptpProcess) ProcessSynceEvents(logEntry synce.LogEntry) {
 		default:
 		}
 	}
-
 }
+
 func (p *ptpProcess) SyncEDeviceByInterface(iface string) *synce.Config {
 	if p.syncERelations != nil {
 		for _, sConfig := range p.syncERelations.Devices {
